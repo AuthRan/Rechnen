104951e81d482306c55a2daf306dcb948cd66222
Ashutosh Ranjan <authran.off@gmail.com>
2025-08-28 04:46:22 +0530

    Completing project

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,12 @@
+import { Button, Result } from 'antd';
+
+import useLanguage from '@/locale/useLanguage';
+
+const About = () => {
+  const translate = useLanguage();
+  return (
+    <h1>We Are Working On It</h1>
+  );
+};
+
+export default About;
